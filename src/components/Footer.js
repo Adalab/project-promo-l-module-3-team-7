a40d0579a645e57610059776b1ecf274dd8da1d3
Eugenia <mariaedallasta@gmail.com>
2021-02-17 15:02:53 -0300

    Add footer branch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
-//import "../style/App.scss";
+import logoAdalab from "../images/logo-adalab.png";
+import "../style/layout/Footer.scss";
 
 function Footer() {
     return (
-        <p>Nuestro Footer</p>
-  );
+      <footer className="footer">
+        <h2 className="footer__text">Awesome profile-cards @2021</h2>
+        <a href="https://adalab.es/" target="__blank">
+          <img
+            src={logoAdalab}
+            className="footer__adalab-logo"
+            alt="Adalab home logo link"
+            title="Adalab logo"
+          />
+        </a>
+      </footer>
+    );
 }
 
 export default Footer;
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,9 +1,54 @@
-//import "../style/App.scss";
+import "../style/layout/Preview.scss";
 
 function Preview() {
     return (
-        <p>Nuestro Preview</p>
-  );
+      <section className="section__card">
+        <div className="section__container">
+          <button className="reset js-reset">
+            <i className="fa fa-trash"></i>Reset
+          </button>
+          <div className="card palette3card js_preview-card">
+            <div className="card__titles">
+              <h3 className="card__titles--name js-preview-name">
+                Nombre apellidos
+              </h3>
+              <h4 className="card__titles--job js-preview-job">
+                Programadora front end
+              </h4>
+            </div>
+            <div className="card__image js__profile-image"></div>
+            <div className="card__links--container">
+              <ul className="logo__list">
+                <li className="logo__list--item js-logo-list hidden">
+                  <a className="link__card js-preview-tel" title="">
+                    <i className="fa fa-mobile" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="logo__list--item js-logo-list hidden">
+                  <a className="link__card js-preview-email" href="" title="">
+                    <i className="fa fa-envelope-o"></i>
+                  </a>
+                </li>
+                <li className="logo__list--item js-logo-list hidden">
+                  <a
+                    className="link__card js-preview-linkedin"
+                    href=""
+                    title=""
+                  >
+                    <i className="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li className="logo__list--item js-logo-list hidden">
+                  <a className="link__card js-preview-github" href="" title="">
+                    <i className="fa fa-github-alt"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default Preview;
