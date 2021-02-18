6d9ffd066f9a9a0df300e37eb436aa153024055e
irenemanas <irenemanas@gmail.com>
2021-02-18 12:50:02 +0100

    add page-column and reset scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import "../style/App.scss";
+import "../style/layout/_card-page.scss";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 
+
+
+
 function App() {
   return (
-    <div className="page">
+    <div className="page__column">
       <Header />
       <Main />
       <Footer />
