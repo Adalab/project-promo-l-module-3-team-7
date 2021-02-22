960b756aca08cc067a1206d986705484cf3e5ad8
Eugenia <mariaedallasta@gmail.com>
2021-02-22 12:47:11 +0100

    Fix merge problem

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -7,25 +7,24 @@ function Preview() {
       <div className="section__container">
         <button className="reset js-reset">
           <i className="fa fa-trash"></i>Reset
-          </button>
+        </button>
         <div className="card palette3card js_preview-card">
           <div className="card__titles">
             <h3 className="card__titles--name js-preview-name">
               Nombre apellidos
-              </h3>
+            </h3>
             <h4 className="card__titles--job js-preview-job">
               Programadora front end
-              </h4>
-            </div>
-            <div className="card__image js__profile-image"></div>
-            <div className="card__links--container">
-              <ul class="logo__list">
-                <Social aClass="tel" iClass="mobile" />
-                <Social aClass="email" iClass="envelope-o" />
-                <Social aClass="linkedin" iClass="linkedin" />
-                <Social aClass="github" iClass="github-alt" />
-             </ul>
-            </div>
+            </h4>
+          </div>
+          <div className="card__image js__profile-image"></div>
+          <div className="card__links--container">
+            <ul class="logo__list">
+              <Social aClass="tel" iClass="mobile" />
+              <Social aClass="email" iClass="envelope-o" />
+              <Social aClass="linkedin" iClass="linkedin" />
+              <Social aClass="github" iClass="github-alt" />
+            </ul>
           </div>
         </div>
       </div>
