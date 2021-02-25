e3f8d15c51e70141c2fbd696e622181c30a7f7ff
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-25 15:54:36 +0100

    resolve conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-
-import "../style/layout/Header.scss";
-import logo from "../images/logo-awesome-profile-cards.svg"
-
-function Header() {
-    return (
-        <header className="header__cards">
-            <div className="header__cards--container">
-                <a className="header__cards--link" href="index.html" alt="landing page link"
-                ><img
-                        className="header__cards--photo"
-                        src={logo}
-                        title="awesome-profile cards logo"
-                    /></a>
-            </div>
-        </header>
-    );
-}
-
-export default Header;
-=======
 import "../style/layout/Header.scss";
 import logo from "../images/logo-awesome-profile-cards.svg";
 
@@ -44,4 +22,3 @@ function Header() {
 }
 
 export default Header;
->>>>>>> ef7a318701b1ce8869841e63becafbbf125fbf6f
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import '../style/layout/Preview.scss';
-import Social from './Social';
-
-function Preview() {
-  return (
-    <section className='section__card'>
-      <div className='section__container'>
-        <button className='reset js-reset'>
-          <i className='fa fa-trash'></i>Reset
-        </button>
-        <div className='card palette3card js_preview-card'>
-          <div className='card__titles'>
-            <h3 className='card__titles--name js-preview-name'>
-              Nombre apellidos
-            </h3>
-            <h4 className='card__titles--job js-preview-job'>
-              Programadora front end
-            </h4>
-          </div>
-          <div className='card__image js__profile-image'></div>
-          <div className='card__links--container'>
-            <ul class='logo__list'>
-              <Social aClass='tel' iClass='mobile' />
-              <Social aClass='email' iClass='envelope-o' />
-              <Social aClass='linkedin' iClass='linkedin' />
-              <Social aClass='github' iClass='github-alt' />
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default Preview;
-=======
 import "../style/layout/Preview.scss";
 import Social from "./Social";
 
@@ -70,4 +33,3 @@ function Preview() {
 }
 
 export default Preview;
->>>>>>> ab01dac06bd27fbb0a56dcd2aca28516a403f7a0
