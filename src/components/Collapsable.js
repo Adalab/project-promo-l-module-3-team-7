a2861f785cd8ff12b1bbcf0c35193f665b19e39f
irenemanas <irenemanas@gmail.com>
2021-02-25 13:44:47 +0100

    add isClosed to collapsable

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,35 +1,23 @@
 import React, { useState } from "react";
 
 function Collapsable(props) {
-  let [isOpen, setOpen] = useState(true);
+  let [isOpen, setOpen] = useState(props.isClosed);
   let [classClose, setClass] = useState("");
   const handleLegendClick = () => {
     setOpen(!isOpen);
     setClass((classClose = isOpen ? "collapsable--close" : ""));
   };
-
+  console.log(props.isClosed);
   return (
-    <fieldset
-      className={`collapsable__${props.fieldset} collapsable ${classClose} js-collapsable`}
-    >
-      <legend
-        className="collapsable__header js-collapsable-header"
-        onClick={handleLegendClick}
-      >
-        <h2 className="tittle__collapsable">
-          <i
-            className={"fa fa-collapsable " + props.icon}
-            aria-hidden="true"
-          ></i>
+    <fieldset className={`collapsable__${props.fieldset} collapsable ${classClose} js-collapsable`}>
+      <legend className='collapsable__header js-collapsable-header' onClick={handleLegendClick}>
+        <h2 className='tittle__collapsable'>
+          <i className={"fa fa-collapsable " + props.icon} aria-hidden='true'></i>
           {props.title}
         </h2>
-        <i className="fa fa-angle-up" aria-hidden="true"></i>
+        <i className='fa fa-angle-up' aria-hidden='true'></i>
       </legend>
-      <div
-        className={`collapsable__content collapsable__content--${props.fieldset}`}
-      >
-        {props.children}
-      </div>
+      <div className={`collapsable__content collapsable__content--${props.fieldset}`}>{props.children}</div>
     </fieldset>
   );
 }
