a01c96134a3ff01c4dc4710e77bc4f1f1b56a881
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-03-01 14:02:43 +0100

    add isClose to form and share collapsables

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,15 @@
-import '../style/layout/_card-page.scss';
-import '../style/layout/_design.scss';
-import '../style/layout/_form.scss';
-import '../style/layout/_share.scss';
-import Collapsable from './Collapsable.js';
-import Palette from './Palette.js';
-import Input from './Input.js';
+import "../style/layout/_card-page.scss";
+import "../style/layout/_design.scss";
+import "../style/layout/_form.scss";
+import "../style/layout/_share.scss";
+import Collapsable from "./Collapsable.js";
+import Palette from "./Palette.js";
+import Input from "./Input.js";
 
 function Form(props) {
   return (
-    <form method='' action='' className='collapsable-container'>
-       <Collapsable
+    <form method="" action="" className="collapsable-container">
+      <Collapsable
         title="Diseña"
         icon="fa-object-ungroup"
         fieldset="design"
@@ -34,89 +34,99 @@ function Form(props) {
           changePalette={props.changePalette}
         />
       </Collapsable>
-      <Collapsable title='Rellena' icon='fa-keyboard-o' fieldset='form'>
-        <div className='form js-form'>
+      <Collapsable
+        title="Rellena"
+        icon="fa-keyboard-o"
+        fieldset="form"
+        isClose={true}
+      >
+        <div className="form js-form">
           <Input
-            name='name'
-            label='Nombre completo'
-            placeholder='Nombre completo'
+            name="name"
+            label="Nombre completo"
+            placeholder="Nombre completo"
             value={props.name}
             handleInput={props.handleInput}
           />
           <Input
-            name='job'
-            label='Puesto'
-            placeholder='Profesión'
+            name="job"
+            label="Puesto"
+            placeholder="Profesión"
             value={props.job}
             handleInput={props.handleInput}
           />
-          <div className='form__display--image'>
+          <div className="form__display--image">
             <div>
-              <label className='form__label js-label-image' htmlFor='photo'>
+              <label className="form__label js-label-image" htmlFor="photo">
                 Imagen de perfil
               </label>
               <button
-                className='btn__submit--image js__profile-trigger'
-                type='submit'
-                value='Anadir imagen'
-                name='photo'
-                id='photo'
+                className="btn__submit--image js__profile-trigger"
+                type="submit"
+                value="Anadir imagen"
+                name="photo"
+                id="photo"
                 required
               >
                 Añadir imagen
               </button>
               <input
-                type='file'
-                name=''
-                className='btn__submit--image action__hiddenField js__profile-upload-btn'
+                type="file"
+                name=""
+                className="btn__submit--image action__hiddenField js__profile-upload-btn"
               />
             </div>
-            <div className='form__preview--image js__profile-preview'></div>
+            <div className="form__preview--image js__profile-preview"></div>
           </div>
           <Input
-            name='email'
-            label='email'
-            placeholder='nombre.apellido@example.com'
-            type='email'
+            name="email"
+            label="email"
+            placeholder="nombre.apellido@example.com"
+            type="email"
             value={props.email}
             handleInput={props.handleInput}
           />
           <Input
-            name='phone'
-            label='Telefono'
-            placeholder='+34 666666666'
-            type='tel'
+            name="phone"
+            label="Telefono"
+            placeholder="+34 666666666"
+            type="tel"
             value={props.tel}
             handleInput={props.handleInput}
           />
 
           <Input
-            name='linkedin'
-            label='Linkedin'
-            placeholder='Nombre de usuario de LinkedIn'
+            name="linkedin"
+            label="Linkedin"
+            placeholder="Nombre de usuario de LinkedIn"
             value={props.linkedin}
             handleInput={props.handleInput}
           />
           <Input
-            name='github'
-            label='Github'
-            placeholder='Nombre de usuario de GitHub'
+            name="github"
+            label="Github"
+            placeholder="Nombre de usuario de GitHub"
             value={props.github}
             handleInput={props.handleInput}
           />
         </div>
       </Collapsable>
-      <Collapsable title='Comparte' icon='fa-share-alt' fieldset='share'>
-        <button className='button__create link_animation js-create-btn'>
-          <i className='fa fa-address-card-o' aria-hidden='true'></i>Crear
+      <Collapsable
+        title="Comparte"
+        icon="fa-share-alt"
+        fieldset="share"
+        isClose={true}
+      >
+        <button className="button__create link_animation js-create-btn">
+          <i className="fa fa-address-card-o" aria-hidden="true"></i>Crear
           tarjeta
         </button>
 
-        <div className='confirm__share js-card-result share-hidden'>
-          <p className='confirm__share--title'>La tarjeta ha sido creada:</p>
-          <a className='confirm__share--link' href=''></a>
+        <div className="confirm__share js-card-result share-hidden">
+          <p className="confirm__share--title">La tarjeta ha sido creada:</p>
+          <a className="confirm__share--link" href=""></a>
         </div>
-        <div className='rectangle'></div>
+        <div className="rectangle"></div>
       </Collapsable>
     </form>
   );
