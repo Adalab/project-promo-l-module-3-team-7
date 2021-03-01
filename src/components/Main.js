41568e7aceec2c984c6e93e7ccc1b5c2a1bd3d40
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-03-01 13:55:07 +0100

    fix handleInput

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,46 +1,44 @@
 //import "../style/App.scss";
-import Form from './Form';
-import Preview from './Preview';
-import '../style/layout/_card-page.scss';
-import React, { useState } from 'react';
+import Form from "./Form";
+import Preview from "./Preview";
+import "../style/layout/_card-page.scss";
+import React, { useState } from "react";
 
 function Main() {
-
   const [selectedPalette, setSelectedPalette] = useState("1");
-  const [name, setName] = useState('Nombre apellidos');
-  const [job, setJob] = useState('Programadora front end')
-  const [email, setEmail] = useState('');
-  const [tel, setTel] = useState('');
-  const [linkedin, setLinkedin] = useState('');
-  const [github, setGithub] = useState('');
+  const [name, setName] = useState("Nombre apellidos");
+  const [job, setJob] = useState("Programadora front end");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [github, setGithub] = useState("");
 
   const handleChangePalette = (ev) => {
     const newValue = ev.currentTarget.value;
 
     setSelectedPalette(newValue);
+  };
+  function handleInput(ev) {
+    const field = ev.target.name;
 
-    function handleInput(ev) {
-      const field = ev.target.name;
-    };
-
-    if (field === 'name') {
+    if (field === "name") {
       console.log(ev.target.value);
       setName(ev.target.value);
-    } else if (field === 'job') {
+    } else if (field === "job") {
       setJob(ev.target.value);
-    } else if (field === 'email') {
+    } else if (field === "email") {
       setEmail(ev.target.value);
-    } else if (field === 'tel') {
+    } else if (field === "tel") {
       setTel(ev.target.value);
-    } else if (field === 'linkedin') {
+    } else if (field === "linkedin") {
       setLinkedin(ev.target.value);
-    } else if (field === 'github') {
+    } else if (field === "github") {
       setGithub(ev.target.value);
     }
   }
 
   return (
-    <main className='main'>
+    <main className="main">
       <Preview
         job={job}
         name={name}
@@ -50,8 +48,11 @@ function Main() {
         github={github}
         selectedPalette={selectedPalette}
       />
-      <Form handleInput={handleInput} selectedPalette={selectedPalette}
-        changePalette={handleChangePalette} />
+      <Form
+        handleInput={handleInput}
+        selectedPalette={selectedPalette}
+        changePalette={handleChangePalette}
+      />
     </main>
   );
 }
