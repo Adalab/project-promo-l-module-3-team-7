2c98de068a061bf520210d09c71581c0982e062c
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-03-16 15:10:09 +0100

    update social.js - color-picker

--- web/src/components/Social.js
+++ web/src/components/Social.js
@@ -3,13 +3,19 @@ import "../style/layout/Preview.scss";
 function Social(props) {
   return (
     // <li className='logo__list--item js-logo-list hidden'>
-    <li className="logo__list--item js-logo-list">
+    <li
+      className="logo__list--item js-logo-list"
+      style={props.color3 ? { border: `2px solid ${props.color3}` } : {}}
+    >
       <a
         className={"link__card js-preview-" + props.aClass}
         title=""
         href={props.href}
       >
-        <i className={"fa fa-" + props.iClass}></i>
+        <i
+          className={"fa fa-" + props.iClass}
+          style={{ color: props.color1 }}
+        ></i>
       </a>
     </li>
   );
